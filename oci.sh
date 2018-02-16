e160bb50ca2e7432ccf65de0b1ada6036dbabcda
Marian Kuna <mkunask@gmail.com>
2018-02-16 16:20:14 +0100

    Commit from Visual Studio Code

--- oci.sh
+++ oci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./oci_curl.sh
 
-#Create instance
+#Create OCI instance
 oci-curl iaas.us-phoenix-1.oraclecloud.com get "/20160918/images?compartmentId=ocid1.compartment.oc1..aaaaaaaaymztafvorbatzvgosdfsujarypdn3nvsj73qklegjzf4fppzbtaq"
